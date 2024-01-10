3e36c42134d02db978bbb1ecf9a7fa0840d00a8c
TeacherArea <sewilius.berg@initialize.se>
2024-01-10 06:46:39 +0100

    adding footer and header to public/pages

--- public_html/index.php
+++ public_html/index.php
@@ -1,4 +1,4 @@
 <?php define("TITLE", "Cafe PHP | WELCOME"); ?>
-<?php include '../resources/templates/header.php'; ?>
+<?php include 'pages/header.php'; ?>
 <?php include 'pages/start.php'; ?>
-<?php include '../resources/templates/footer.php'; ?>
+<?php include 'paes/footer.php'; ?>
--- resources/settings/dbsettings.php
+++ resources/settings/dbsettings.php
@@ -3,7 +3,7 @@
     define('DB_HOST', 'localhost');
     define('DB_USER', 'root');
     define('DB_PASS', '');
-    define('DB_NAME', 'cafephp');
+    define('DB_NAME', 'cafe_php_v1');
     define('DB_TIMEZON', 'Europe/Stockholm');
     define('DB_CHARSET', 'utf-8');
 ?>
